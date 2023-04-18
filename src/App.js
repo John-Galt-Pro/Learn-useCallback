db8d358b5fda8f55ab1a3265c60e99486db83e6b
John_Galt <john.galt.prog@gmail.com>
2023-04-18 14:13:02 +0300

    show problem without useEffect

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import {Component, useState, useEffect} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+const getSomeImages = () => {
+    console.log('fetching');
+    return [
+        'https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/rolls_royce_phantom_top_10.jpg?itok=XjL9f1tx',
+        'https://www.supercars.net/blog/wp-content/uploads/2022/09/Best-New-Sports-and-Performance-Cars-2022_Chevrolet_Corvette.jpg'
+    ]
+}
+
+const Slider = (props) => {
+
+    const [slide, setSlide] = useState(0);
+
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
+    }
+
+    const [autoplay, setAutoplay] = useState(false);
+
+    function toggleAutoplay() {
+        setAutoplay(autoplay => !autoplay)
+    }
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                {
+                    getSomeImages().map( (url,i) => {
+                        return (
+                            <img key={i} className="d-block w-100" src={url} alt="slide" />
+                        )
+                    } )
+                }
+
+                <div className="text-center mt-5">Active slide {slide} <br/>
+                    {autoplay ? 'auto' : null}
+                </div>
+                <div className="buttons mt-3">
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={toggleAutoplay}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+function App() {
+    const [slider, setSlider] = useState(true);
+
+
+  return (
+    <>
+        <button onClick={() => setSlider(false)}>Click</button>
+        {slider ? <Slider/> : null}
+    </>
+  );
+}
+
+export default App;
+
+
